41fa5fc7426953345f34c583e975fe981d088dce
gdh1995 <gdh1995@qq.com>
2015-12-21 16:06:15 +0800

    remove `with`

--- math_parser.js
+++ math_parser.js
@@ -270,11 +270,6 @@ var MathParser = (function () {
 			}
 
 			return vars;
-		},
-
-		toJSFunction: function (param, variables) {
-			var f = new Function(param, "with(MathParser.values) { return " + this.simplify(variables).toString(true) + "; }");
-			return f;
 		}
 	};
 
